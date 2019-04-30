3bcc0eeecd62a388267b0e1f6b177f99e942a4d4
Ben-Dudley <vilenaguslyakova@gmail.com>
2019-04-30 19:40:11 +0300

    GNL

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bdudley <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/05 21:06:05 by bdudley           #+#    #+#             */
+/*   Updated: 2019/03/02 19:05:05 by bdudley          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+
+int		get_next_line(const int fd, char **line);
+# define BUFF_SIZE 2
+#endif
